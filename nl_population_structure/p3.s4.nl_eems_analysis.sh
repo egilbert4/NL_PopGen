6acc95cbb819948bc842c68bfeb52fccc5decef3
Edmund Gilbert <32327302+egilbert4@users.noreply.github.com>
2022-11-29 17:22:48 +0000

    Update p3.s4.nl_eems_analysis.sh

--- nl_population_structure/p3.s4.nl_eems_analysis.sh
+++ nl_population_structure/p3.s4.nl_eems_analysis.sh
@@ -1,1 +1,9 @@
-# something
+#!/bin/bash
+
+# move to the EEMS cluster and convert the plink data into the diffs file
+
+# variables
+sequence=NL-ancestry
+
+# use bed2diffs to convert
+bed2diffs_v1 --bfile $sequence.geog-filtered --nthreads 4
