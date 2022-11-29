0ce7d8c08f15f6e536c028dc1a31842733056995
Edmund Gilbert <32327302+egilbert4@users.noreply.github.com>
2022-11-29 17:26:31 +0000

    Update p3.s4.nl_eems_analysis.sh

--- nl_population_structure/p3.s4.nl_eems_analysis.sh
+++ nl_population_structure/p3.s4.nl_eems_analysis.sh
@@ -1,9 +1,60 @@
 #!/bin/bash
 
-# move to the EEMS cluster and convert the plink data into the diffs file
-
+###############################################################################
+## Convert the plink data into the diffs file
 # variables
 sequence=NL-ancestry
 
 # use bed2diffs to convert
 bed2diffs_v1 --bfile $sequence.geog-filtered --nthreads 4
+
+###############################################################################
+# perform the initial EEMS analysis
+
+# variables
+ndemes=300
+
+mkdir intial/
+
+# create param files
+for chain in {1..10}
+  do
+  echo -e "datapath = $sequence.geog-filtered
+  mcmcpath = initial/$sequence.EEMS-initial-nDemes${ndemes}.chain${chain}
+  nIndiv = 739
+  nSites = 685221
+  nDemes = ${ndemes}
+  diploid = true
+  numMCMCIter = 5000000
+  numBurnIter = 3000000
+  numThinIter = 9999" > $sequence.EEMS-initial-nDemes${ndemes}.chain${chain}.param
+
+  # run EEMS
+  runeems_snps --params $sequence.EEMS-initial-nDemes${ndemes}.chain${chain}.param
+  done
+
+###############################################################################
+# perform the extension EEMS analysis, make sure this is using the EEMS cluster
+# variables
+mkdir extension/
+
+ndemes=300
+start_chain=5
+
+# create param file
+for chain in {1..10}
+  do
+  echo -e "datapath = $sequence.geog-filtered
+  mcmcpath = extension/$sequence.EEMS-extension-nDemes${ndemes}.chain${chain}
+  prevpath = initial/$sequence.EEMS-initial-nDemes${ndemes}.chain${start_chain}
+  nIndiv = 739
+  nSites = 685221
+  nDemes = ${ndemes}
+  diploid = true
+  numMCMCIter = 3000000
+  numBurnIter = 2000000
+  numThinIter = 9999" > $sequence.EEMS-extension-nDemes${ndemes}.chain${chain}.param
+
+  # run EEMS
+  runeems_snps --params $sequence.EEMS-extension-nDemes${ndemes}.chain${chain}.param
+  done
